f714377a335edc77bb42eb9519e9c165b50e8508
albert-santos01 <albertsm002@gmail.com>
2025-02-13 18:55:38 +0100

    change directory for testing soundnet

--- scripts/test_Flickr10k.sh
+++ scripts/test_Flickr10k.sh
@@ -2,5 +2,5 @@ python -u main.py \
 --batch_size 1 --gpus 1 \
 --exp_name SSL-TIE \
 --dataset_mode Flickr \
---soundnet_test_path "dir_of_SoundNet_Test_Data/" \
+--soundnet_test_path "$DATA/dir_of_SoundNet_Test_Data/" \
 --test "ckpts/flickr10k.pth.tar"

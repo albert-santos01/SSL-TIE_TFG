49d6bebc90bac5810cfe355bf0010da592ec5dd6
albert-santos01 <albertsm002@gmail.com>
2025-03-18 14:09:50 +0100

    clean prints

--- main_efficient.py
+++ main_efficient.py
@@ -201,17 +201,12 @@ def train_one_epoch(train_loader, model, criterion, optim, device, epoch, args):
             losses_ts.update(loss_ts.item(), B) 
 
 
-        print(imgs_out.requires_grad)
-        # print(img)
-        print(loss.requires_grad)
-        print(loss.grad_fn)
-
+        
 
         #TODO: Check if loss is really able to backward
         
         optim.zero_grad()
         loss.backward()
-
         optim.step()
 
         batch_time.update(time.time() - end)
@@ -297,24 +292,24 @@ def validate(val_loader, model, criterion, device, epoch, args):
                     img_org = im[random_sample]
                     vis_matchmap(matchmap_array,img_org,video_name=f'epoch_{epoch}',epoch=epoch,save_dir=args.img_path,args=args)
         
-        
-    #     recalls = calc_recalls(image_output, audio_output, nframes, simtype=args.simtype)
-    #     A_r10 = recalls['A_r10']
-    #     I_r10 = recalls['I_r10']
-    #     A_r5 = recalls['A_r5']
-    #     I_r5 = recalls['I_r5']
-    #     A_r1 = recalls['A_r1']
-    #     I_r1 = recalls['I_r1']
+    """    
+        recalls = calc_recalls(image_output, audio_output, nframes, simtype=args.simtype)
+        A_r10 = recalls['A_r10']
+        I_r10 = recalls['I_r10']
+        A_r5 = recalls['A_r5']
+        I_r5 = recalls['I_r5']
+        A_r1 = recalls['A_r1']
+        I_r1 = recalls['I_r1']
 
-    # print(' * Audio R@10 {A_r10:.3f} Image R@10 {I_r10:.3f} over {N:d} validation pairs'
-    #       .format(A_r10=A_r10, I_r10=I_r10, N=N_examples), flush=True)
-    # print(' * Audio R@5 {A_r5:.3f} Image R@5 {I_r5:.3f} over {N:d} validation pairs'
-    #       .format(A_r5=A_r5, I_r5=I_r5, N=N_examples), flush=True)
-    # print(' * Audio R@1 {A_r1:.3f} Image R@1 {I_r1:.3f} over {N:d} validation pairs'
-    #       .format(A_r1=A_r1, I_r1=I_r1, N=N_examples), flush=True)
+    print(' * Audio R@10 {A_r10:.3f} Image R@10 {I_r10:.3f} over {N:d} validation pairs'
+          .format(A_r10=A_r10, I_r10=I_r10, N=N_examples), flush=True)
+    print(' * Audio R@5 {A_r5:.3f} Image R@5 {I_r5:.3f} over {N:d} validation pairs'
+          .format(A_r5=A_r5, I_r5=I_r5, N=N_examples), flush=True)
+    print(' * Audio R@1 {A_r1:.3f} Image R@1 {I_r1:.3f} over {N:d} validation pairs'
+          .format(A_r1=A_r1, I_r1=I_r1, N=N_examples), flush=True)
 
-    # return recalls
-                    
+    return recalls
+       """             
     
     wandb.log({"val_loss": losses.avg, "epoch": epoch}) if args.use_wandb else None
     print('Epoch: [{0}]\t Eval '

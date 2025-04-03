3d37af17f85e9e33ec8f875b033ca75b329fea2b
albert-santos01 <albert.santos01@estudiant.upf.edu>
2025-04-03 15:23:39 +0200

    add DAVENet spectrogram processing

--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -13,6 +13,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
 import torchaudio
+import librosa
 import torchaudio.transforms as audio_T
 import pdb
 import time
@@ -58,6 +59,7 @@ class AVDataset(ABC, Dataset):
         self._init_transform()
         self._init_atransform()
         self.video_files = []
+        self.audio_conf = {}
 
     def _init_transform(self):
         mean = [0.485, 0.456, 0.406]
@@ -126,17 +128,65 @@ class AVDataset(ABC, Dataset):
     def _get_audio(self,file):
         pass
 
+    def preemphasis(self,signal,coeff=0.97):
+        """perform preemphasis on the input signal.
+        
+        :param signal: The signal to filter.
+        :param coeff: The preemphasis coefficient. 0 is none, default 0.97.
+        :returns: the filtered signal.
+        """    
+        return np.append(signal[0],signal[1:]-coeff*signal[:-1])
     
-    def __getitem__(self, index):
-        # file= self.video_files[index]
-        file = self._get_file(index)
-        frame_path = self._get_video(file)
-        audio_path = self._get_audio(file)
+    def _load_audio_DAVENet(self,file):
+        audio_type = self.audio_conf.get('audio_type', 'melspectrogram')
+        if audio_type not in ['melspectrogram', 'spectrogram']:
+            raise ValueError('Invalid audio_type specified in audio_conf. Must be one of [melspectrogram, spectrogram]')
+        preemph_coef = self.audio_conf.get('preemph_coef', 0.97)
+        sample_rate = self.audio_conf.get('sample_rate', 16000)
+        window_size = self.audio_conf.get('window_size', 0.025)
+        window_stride = self.audio_conf.get('window_stride', 0.01)
+        window_type = self.audio_conf.get('window_type', 'hamming')
+        num_mel_bins = self.audio_conf.get('num_mel_bins', 40)
+        target_length = self.audio_conf.get('target_length', 2048)
+        use_raw_length = self.audio_conf.get('use_raw_length', False)
+        padval = self.audio_conf.get('padval', 0)
+        fmin = self.audio_conf.get('fmin', 20)
+        n_fft = self.audio_conf.get('n_fft', int(sample_rate * window_size))
+        win_length = int(sample_rate * window_size)
+        hop_length = int(sample_rate * window_stride)
 
-        frame = self.img_transform(self._load_frame( frame_path ))
-        frame_ori = np.array(self._load_frame(frame_path))
+        # load audio, subtract DC, preemphasis
+        y, sr = librosa.load(file, sample_rate)
+        if y.size == 0:
+            y = np.zeros(200)
+        y = y - y.mean()
+        y = self.preemphasis(y, preemph_coef)
+        # compute mel spectrogram
+        stft = librosa.stft(y, n_fft=n_fft, hop_length=hop_length,
+            win_length=win_length,
+            window=self.windows.get(window_type, self.windows['hamming']))
+        spec = np.abs(stft)**2
+        if audio_type == 'melspectrogram':
+            mel_basis = librosa.filters.mel(sr, n_fft, n_mels=num_mel_bins, fmin=fmin)
+            melspec = np.dot(mel_basis, spec)
+            logspec = librosa.power_to_db(melspec, ref=np.max)
+        elif audio_type == 'spectrogram':
+            logspec = librosa.power_to_db(spec, ref=np.max)
+        n_frames = logspec.shape[1]
+        if use_raw_length:
+            target_length = n_frames
+        p = target_length - n_frames
+        if p > 0:
+            logspec = np.pad(logspec, ((0,0),(0,p)), 'constant',
+                constant_values=(padval,padval))
+        elif p < 0:
+            logspec = logspec[:,0:p]
+            n_frames = target_length
+        logspec = torch.FloatTensor(logspec)
+        return logspec, n_frames
 
-        samples, samplerate = torchaudio.load(audio_path)
+    def _load_audio_SSL_TIE(self,file):
+        samples, samplerate = torchaudio.load(file)
 
         if samples.shape[1] < samplerate * 10:
             n = int(samplerate * 10 / samples.shape[1]) + 1
@@ -163,6 +213,22 @@ class AVDataset(ABC, Dataset):
             spectrogram = maskings(spectrogram)
 
         spectrogram = self.AmplitudeToDB(spectrogram)
+
+        return spectrogram
+    
+    def __getitem__(self, index):
+        # file= self.video_files[index]
+        file = self._get_file(index)
+        frame_path = self._get_video(file)
+        audio_path = self._get_audio(file)
+
+        frame = self.img_transform(self._load_frame( frame_path ))
+        frame_ori = np.array(self._load_frame(frame_path))
+
+        if self.args.spec_DAVENet:
+            spectrogram = self._load_audio_DAVENet(audio_path)
+        else:
+            spectrogram = self._load_audio_SSL_TIE(audio_path)
              
         if self.args.val_video_idx == index:
             audio = audio_path

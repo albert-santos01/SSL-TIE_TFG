7a75417405cd2ad225983405226b1ea4da5cb72d
albert-santos01 <albertsm002@gmail.com>
2025-04-26 14:42:37 +0200

    add --MISA_2_LVS

--- opts.py
+++ opts.py
@@ -58,6 +58,7 @@ def get_arguments():
 
     parser.add_argument('--SISA_2_MISA_step', default=0, type=int, help="Threshold to Change to MISA at certain step of the first epoch")
     parser.add_argument('--SISA_2_MISA_epoch', default=0,type=int, help="Threshold to Change to MISA at certain epoch")
+    parser.add_argument('--MISA_2_LVS_epoch', default=0,type=int, help="Given that simtype is MISA, change to LVS at certain epoch")
 
     parser.add_argument('--spec_DAVENet', action='store_true', help= "Load the audios as DAVENet configuration")
     parser.set_defaults(spec_DAVENet=False)

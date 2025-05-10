36247df336c93d668542cbad70b0de1d5356dad5
albert-santos01 <albert.santos01@estudiant.upf.edu>
2025-05-10 17:31:56 +0200

    add that the videos are normalized across all the frames

--- opts.py
+++ opts.py
@@ -49,7 +49,7 @@ def get_arguments():
     parser.add_argument('--get_nFrames', action='store_true', help='Pass the number of frames till the em')
     parser.set_defaults(get_nFrames=False)
     parser.add_argument('--normalize_volumes_thw', action='store_true', help='Normalize the similarity volumes to [0,1] across the T, H, W dimensions')
-    parser.set_defaults(normalize_volumes_thw=False)
+    parser.set_defaults(normalize_volumes_thw=True)
 
     parser.add_argument('--placesAudio', default=None, type=str, help='Root directory path of metadata PlacesAudio')
     parser.add_argument('--mem_efficient', action='store_true', help='Use cuda')
